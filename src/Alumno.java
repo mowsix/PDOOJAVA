6f7a61cce5284f2dddb247e2ffa0f12c58dba00e
Santiago Gallego H <santiago.gallegoh@upb.edu.co>
2023-02-05 13:53:17 -0500

    metodos y constructores

---
+++ src/Alumno.java
@@ -0,0 +1,48 @@
+//Crear clase con sus atributos
+
+public class Alumno {
+    int ID;
+    String nombre;
+    String apellido;
+
+    //METODOS
+
+    public void mostrarNombre(){
+        System.out.println("Mi nombre es pedro");
+    }
+    public void saberAprobado(double calificacion){
+        if(calificacion>=3){
+            System.out.println("APROBADOOO");
+        }else {
+            System.out.println("Perdiste :c");
+        }
+    }
+
+    // Constructores
+    public Alumno(int ID, String nombre, String apellido) {
+        this.nombre = nombre;
+        this.ID= ID;
+        this.apellido= apellido;
+    }
+    //Getters y Setters siempre despues de los constructores
+    public int getID() {
+        return ID;
+    }
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
--- src/JAVAPOO.java
+++ src/JAVAPOO.java
@@ -1,5 +1,10 @@
 public class JAVAPOO {
     public static void main(String[] args) {
+        // Crear un objeto de la clase alumno
+        Alumno alumno1= new Alumno(5014,"Santiago","Gallego");
+
+        // Acceder a los metodos dde las clases
+        System.out.println("El ID del alumno 1 es: "+ alumno1.getID());
 
     }
 }
